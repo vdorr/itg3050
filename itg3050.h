e523259f16a56fdfc24c0b975b02497f8d5fc77c
vd <v.dorrich@gmail.com>
2012-06-12 19:57:41 +0200

    hello world

--- itg3050.h
+++ itg3050.h
@@ -8,6 +8,9 @@
 //Revision: 1.1
 //Release Date: 05/19/2011
 
+//interface
+#define I2C_ADDRESS_0	0x68
+#define I2C_ADDRESS_1	0x69
 
 #define WHO_AM_I	0x0
 #define X_OFFS_USRH	0x0c
@@ -43,4 +46,71 @@
 #define FIFO_R		0x3c
 #define USER_CTRL	0x3d
 #define PWR_MGM		0x3e
+
+//WHO_AM_I
+#define I2C_IF_ DIS	7
+#define ID_MASK		0x7e
+#define ID_POS		1
+#define ID_VALUE	0x68
+
+//FIFO_EN
+#define TEMP_OUT	7
+#define GYRO_XOUT	6
+#define GYRO_YOUT	5
+#define GYRO_ZOUT	4
+#define AUX_XOUT	3
+#define AUX_YOUT	2
+#define AUX_ZOUT	1
+#define FIFO_FOOTER	0
+
+//AUX_VDDIO
+#define AUX_VDDIO	2
+
+//AUX_SLV_ADDR
+#define CLKOUT_EN	7
+#define AUX_ID_MASK	0x7f
+
+//DLPF_FS_SYNC
+#define EXT_SYNC_SET2	7
+#define EXT_SYNC_SET1	6
+#define EXT_SYNC_SET0	5
+#define FS_SEL1		4
+#define FS_SEL0		3
+#define DLPF_CFG2	2
+#define DLPF_CFG1	1
+#define DLPF_CFG0	0
+
+//INT_CFG
+#define ACTL			7
+#define OPEN			6
+#define LATCH_INT_EN		5
+#define INT_ANYRD_2CLEAR	4
+#define ITG_RDY_EN		2
+#define RAW_RDY_EN		0
+
+//INT_STATUS
+#define ITG_RDY		2
+#define RAW_DATA_RDY	0
+
+//FIFO_COUNTH
+#define FIFO_COUNT_H_MASK	0x03
+
+//USER_CTRL
+#define FIFO_ EN	6
+#define AUX_IF_EN	5
+#define AUX_IF_RST	3
+#define FIFO_RST	1
+#define GYRO_RST	0
+
+//PWR_MGM
+#define H_RESET		7
+#define SLEEP		6
+#define STBY_XG		5
+#define STBY_YG		4
+#define STBY_ZG		3
+#define CLK_SEL2	2
+#define CLK_SEL1	1
+#define CLK_SEL0	0
+
+
 
--- itg3050.ino
+++ itg3050.ino
@@ -1,1 +1,36 @@
 
+//June 12 2012
+//This program is given to you without any warranty.
+//The author disclaims copyright to this source code.
+
+#include <Wire.h>
+#include <stdint.h>
+#include "itg3050.h"
+
+#define GYRO_ADDR  I2C_ADDRESS_0
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  delay(100);
+  Serial.println(itg3050_detect(GYRO_ADDR) ? "ITG-3050 not found :-(" :  "ITG-3050 found :-)");
+}
+
+void loop()
+{}
+
+int itg3050_detect(uint8_t i2c_addr)
+{
+  Wire.beginTransmission(i2c_addr);
+  Wire.write((uint8_t)WHO_AM_I);
+  Wire.endTransmission();
+  Wire.requestFrom((uint8_t)i2c_addr, (uint8_t)1);
+  if (Wire.available())
+  { 
+    uint8_t hello = Wire.read();
+    return (hello & ID_MASK) != ID_VALUE;
+  }
+  return -1;
+
+}
