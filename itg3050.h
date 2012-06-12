0e0e8e2ecbe804d64983b552e839265c15203c10
vd <v.dorrich@gmail.com>
2012-06-12 18:05:28 +0200

    initial commit

---
+++ itg3050.h
@@ -0,0 +1,46 @@
+
+//June 12 2012
+//This program is given to you without any warranty.
+//The author disclaims copyright to this source code.
+
+//based on RM-ITG-3050.pdf
+//Document Number: RM-ITG-3050-00
+//Revision: 1.1
+//Release Date: 05/19/2011
+
+
+#define WHO_AM_I	0x0
+#define X_OFFS_USRH	0x0c
+#define X_OFFS_USRL	0x0d
+#define Y_OFFS_USRH	0x0e
+#define Y_OFFS_USRL	0x0f
+#define Z_OFFS_USRH	0x10 
+#define Z_OFFS_USRL	0x11
+#define FIFO_EN		0x12
+#define AUX_VDDIO	0x13
+#define AUX_SLV_ADDR	0x14
+#define SMPLRT_DIV	0x15
+#define DLPF_FS_SYNC	0x16
+#define INT_CFG		0x17
+#define AUX_ADDR	0x18
+#define INT_STATUS	0x1a
+#define TEMP_OUT_H	0x1b
+#define TEMP_OUT_L	0x1c
+#define GYRO_XOUT_H	0x1d
+#define GYRO_XOUT_L	0x1e
+#define GYRO_YOUT_H	0x1f
+#define GYRO_YOUT_L	0x20
+#define GYRO_ZOUT_H	0x21
+#define GYRO_ZOUT_L	0x22
+#define AUX_XOUT_H	0x23
+#define AUX_XOUT_L	0x24
+#define AUX_YOUT_H	0x25
+#define AUX_YOUT_L	0x26
+#define AUX_ZOUT_H	0x27
+#define AUX_ZOUT_L	0x28
+#define FIFO_COUNTH	0x3a
+#define FIFO_COUNTL	0x3b
+#define FIFO_R		0x3c
+#define USER_CTRL	0x3d
+#define PWR_MGM		0x3e
+
